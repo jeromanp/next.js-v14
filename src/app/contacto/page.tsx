d29257a249c24042da3b8884b382716b4d57a87a
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-05-16 23:50:00 -0600

    metadata static

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,5 +1,13 @@
-export default function Contacto () {
-    return (
-        <h1>Contacto</h1>
-    )
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contacto",
+  description: "Estas en la sección de contacto",
+};
+export default function Contacto() {
+  return (
+    <>
+      <h1>Contacto</h1>
+    </>
+  );
 }
