0f2b4a22cd37dca575be86e40b7f7b0335e5e4ec
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-05-17 00:36:17 -0600

    title absolute

--- src/app/acerca/page.tsx
+++ src/app/acerca/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata:Metadata = {
+    title:{
+        absolute:"Acerca",        
+    },
+    description:"Estas en la sección Acerca"
+}
+
 export default function Acerca () {
     return (
         <h1>Acerca</h1>
