162ac42890682874723915fcbd5983fe051d4fcc
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-05-17 14:44:10 -0600

    use useRouter from Nextjs

--- src/app/(cuenta)/login/page.tsx
+++ src/app/(cuenta)/login/page.tsx
@@ -1,5 +1,15 @@
-export default function Login () {
-    return (
-        <h1>Inicia Sesión</h1>
-    )
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function Login() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/");
+  };
+  return (
+    <>
+      <h1>Inicia Sesión</h1>
+      <button onClick={handleClick}>Regresar al Inicio</button>
+    </>
+  );
 }
