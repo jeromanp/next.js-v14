ca027a72c2b6092d32049501dd3169d9ec420230
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-05-16 23:50:21 -0600

    metadata dynamic

--- src/app/productos/[productId]/page.tsx
+++ src/app/productos/[productId]/page.tsx
@@ -1,9 +1,17 @@
-export default function ProductDetail({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: {
     productId: string;
   };
-}) {
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Producto ${params.productId}`,
+  };
+};
+
+export default function ProductDetail({ params }: Props) {
   return <h1>Producto {params.productId}</h1>;
 }
